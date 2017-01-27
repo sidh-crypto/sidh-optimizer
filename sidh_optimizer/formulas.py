bbee4e01315f45a3777ace51fe669069517d160b
Luca De Feo <defeo@pippin>
2017-01-27 03:03:47 +0100

    Added generic isogeny formula for Edwards curves

--- sidh_optimizer/formulas.py
+++ sidh_optimizer/formulas.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 
-# Copyright (c) 2016 Luca De Feo.
+# Copyright (c) 2016-2017 Luca De Feo.
 
 from collections import OrderedDict, namedtuple
+import uuid
 import sympy
 
 class Cost():
@@ -25,7 +26,8 @@ class Cost():
     __repr__ = __str__
 
     def __add__(self, other):
-        assert type(other) == type(self)
+        if not isinstance(other, Cost):
+            return NotImplemented
         return Cost(other, **self.costs)
 
     def __neg__(self):
@@ -35,8 +37,9 @@ class Cost():
         return self + (-other)
 
     def __mul__(self, scalar):
-        assert type(scalar) == int
-        return Cost(**{ k:scalar*v for (k,v) in self.costs.items() })
+        if not isinstance(scalar, int):
+            return NotImplemented
+        return Cost(**{ k:int(scalar)*v for (k,v) in self.costs.items() })
 
     __rmul__ = __mul__
     
@@ -64,7 +67,7 @@ Party = namedtuple('Party', ['mul', 'isogeny', 'next_curve'])
 # for operations in GF(p^2)
 A = Cost(a=2)                   # Addition
 M = Cost(m=3, a=4, mod=2)       # Multiplication
-m = Cost(m=2, mod=2)            # Scalar multipliccation
+m = Cost(m=2, mod=2)            # Scalar multiplication
 S = Cost(m=2, a=3, mod=2)       # Squaring
 I = Cost(i=1, m=4, a=1, mod=2)  # Inversion
 
@@ -108,6 +111,7 @@ class Formula():
        self.op = op
        self.children = children
        self.formula = formula
+       self._tag = None
 
     def __add__(self, other):
         if not isinstance(other, Formula):
@@ -122,7 +126,7 @@ class Formula():
     def __mul__(self, other):
         if other is self:
             return Formula('^2', [self, other], self.formula**2)
-        if isinstance(other, Formula):
+        elif isinstance(other, Formula):
             return Formula('*', [self, other], self.formula * other.formula)
         elif type(other) == int:
             if other < 0:
@@ -167,10 +171,17 @@ class Formula():
     def __repr__(self):
         return repr(self.formula)
 
-    def cost(self, costs=None):
+    def cost(self, costs=None, tag=None):
         if costs is None:
-            costs = { '+': A, '-': A, 'u-': Cost(), '*': M, '^2': S, '/': M + I, }
-        return sum((c.cost(costs) for c in self.children), costs[self.op])
+            costs = { '+': Cost(A=1), '-': Cost(A=1), 'u-': Cost(),
+                      '*': Cost(M=1), '^2': Cost(S=1), '/': Cost(M=1, I=1) }
+        # Topological sort: count this node only if it has not been counted yet.
+        # Soooo hacky!
+        if tag is None:
+            tag = uuid.uuid4()
+        cost = (self._tag != tag)*costs[self.op]
+        self._tag = tag
+        return sum((c.cost(costs, tag) for c in self.children), cost)
 
 
 class Var(Formula):
@@ -178,10 +189,124 @@ class Var(Formula):
         self.formula = sympy.symbols(name)
         self.comment = comment
 
-    def cost(self, costs=None):
+    def cost(self, *args, **kwds):
+        return Cost()
+    
+class Const(Formula):
+    def __init__(self, value):
+        self.formula = sympy.numbers.sympify(value)
+
+    def cost(self, *args, **kwds):
         return Cost()
 
-        
-X = Var('X')
-Y = Var('Y')
-Z = Var('Z')
+def cost(*args, costs=None, tag=None):
+    '''
+    Compute cost of formulas, sharing common subexpressions.
+    '''
+    if tag is None:
+        tag = uuid.uuid4()
+    return sum((f.cost(costs, tag) for f in args), Cost())
+    
+
+##########################################
+# Isogenies of Edwards curves
+#
+# To avoid inversions, we use the following generalized Edwards curves
+#
+#    (x² + y²)E = E + Dx²y²
+#
+# Setting d = D/E we recover the usual form for E. curves.
+##########################################
+def ed_double(X, Y, Z):
+    '''
+    <https://hyperelliptic.org/EFD/g1p/auto-edwards-projective.html#doubling-dbl-2007-bl>
+
+    Opcount: 3M + 4S + 6A
+    '''
+    b = (X + Y)^2
+    c = X^2
+    d = Y^2
+    e = c + d
+    h = Z^2
+    j = e - 2*h
+    return (b-e)*j, e*(c-d), e*j
+
+def ed_add(X1, Y1, Z1, X2, Y2, Z2, D, E):
+    '''
+    <https://hyperelliptic.org/EFD/g1p/auto-edwards-projective.html#addition-add-2007-bl>
+    plus minor modifications
+
+    Opcount: 13M + 1S + 7A
+    '''
+    a = Z1*Z2
+    b = a^2
+    ea = E*a
+    eb = E*b
+    c = X1*X2
+    d = Y1*Y2
+    e = D*c*d
+    f = eb - e
+    g = eb + e
+    return ea*f*((X1 + Y1)*(X2 + Y2) - c - d), ea*g*(d-c), f*g
+
+def edwards(ell, D, E, KX, KY, KZ, X, Y, Z):
+    '''
+    Compute generic formula for isogenies of Edwards curves of
+    degree ell.
+
+    Uses Sec. 4.3 of
+    <https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/edwardsisogenies.pdf>
+
+    D, E: curve parameters
+    KX, KY, KZ: kernel generator
+    X, Y, Z: function coordinates
+    '''
+    assert ell % 2 == 1
+    
+    ### Next curve
+    
+    # compute kernel points
+    # Depending on ell, some additions may be replaced with doublings
+    kernel = [(KX, KY, KZ)]
+    for i in range(1, ell//2):
+        tmpX, tmpY, tmpZ = kernel[-1]
+        kernel.append(ed_add(tmpX, tmpY, tmpZ, KX, KY, KZ, D, E))
+
+    # compute constants appearing in isogeny formula (3)
+    D4, E4 = 1, 1
+    constants = []
+    for PX, PY, PZ in kernel:
+        x2, y2, z2 = PX^2, PY^2, PZ^2
+        y4, z4 = y2^2, z2^2
+        D4, E4 = D4*y4, E4*z4
+        tmp = z4 * E
+        constants.append((y2*tmp, x2*tmp, x2*y4*D))
+
+    ### Isogeny evaluation
+    # This uses the constants defined in the previous phase,
+    # named α, β, γ
+    x, y, z = X, Y, Z
+    X2, Y2, Z2 = X^2, Y^2, Z^2
+    Z4 = Z2^2
+    X2Y2 = X2 + Y2 - Z4
+    sub = lambda n: ''.join(chr(ord(c)+ord('₀')-ord('0')) for c in str(n))
+    for i, _ in enumerate(constants):
+        a = c = Var('β' + sub(i))
+        b = Var('α' + sub(i))
+        d = Var('γ' + sub(i))
+        e, f, g = (a - b)*(X2 + Y2), a*X2, b*Y2
+        x *= f - g
+        y *= e - f + g
+        z *= c*Z4 - d*X2Y2
+    Z2S = Z2^(ell // 2)
+    x *= Z2S
+    y *= Z2S
+
+    return (D4**2 * D**ell, E4**2 * E**ell), constants, (x, y, z)
+
+##########################################
+# Isogenies of Montgomery curves
+# <http://eprint.iacr.org/2011/506.pdf>
+##########################################
+def montgomery(ell):
+    pass
